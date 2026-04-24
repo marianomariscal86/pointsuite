e0952e784551089880b91d11b4e4868b1d2f46b9
marianomariscal86 <marianomariscal@gmail.com>
2026-04-24 12:08:33 -0600

    fix supabase

--- src/supabase.js
+++ src/supabase.js
@@ -1,1 +1,6 @@
-export const supabase = { from: () = select: () = eq: () = eq: () = single: () = data: null, error: true }) }) }) }) }) } 
+import { createClient } from '@supabase/supabase-js';
+
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
